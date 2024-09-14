012c981169d7bd9127fe8884c491fb573112a71c
Bruno Yuri <bruno.orle@gmail.com>
2024-09-14 15:01:00 -0300

    teste

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -35,8 +35,5 @@ describe('greeter function', () => {
     );
   });
 
-  // Assert greeter result
-  it('greets a user with `Hello, {name}` message', () => {
-    expect(hello).toBe(`Hello, ${name}`);
-  });
+
 });
